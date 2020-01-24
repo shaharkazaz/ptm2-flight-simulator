af939309acb412878340230015444e773ad58ac4
Shahar Kazaz <shahar.kazaz@gmail.com>
2020-01-24 14:36:22 +0200

    Fix MyInterpreter return value missing + finish ex4

--- src/test/MyInterpreter.java
+++ src/test/MyInterpreter.java
@@ -1,8 +1,11 @@
 package test;
 
+import client_side.Parser;
+
 public class MyInterpreter {
 
     public static int interpret(String[] lines) {
         // call your interpreter here - call Parser / Lexer
+        return Parser.getInstance().parseScript(lines);
     }
 }
